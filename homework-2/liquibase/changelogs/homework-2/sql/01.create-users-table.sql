06fd3a0130a056d09680614faa233acc0f199e66
Deterome <rodion5rodion@gmail.com>
2024-08-08 22:42:43 +0300

    Added UUID to roles table

--- homework-2/liquibase/changelogs/homework-2/sql/01.create-users-table.sql
+++ homework-2/liquibase/changelogs/homework-2/sql/01.create-users-table.sql
@@ -1,6 +1,6 @@
 CREATE TABLE users (
      user_id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
-     role_id INTEGER,
+     role_id UUID,
      username VARCHAR(255),
      hashed_password VARCHAR(255),
      FOREIGN KEY (role_id) REFERENCES roles (role_id)
