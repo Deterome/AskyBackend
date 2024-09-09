6646e1e8924c32465747d026eadea1a41e323579
Deterome <rodion5rodion@gmail.com>
2024-09-10 00:58:13 +0300

    deleted unnecessary files

--- homework-6/spring-orm/src/main/liquibase/changelogs/v0.0.2/sql/00.create-users_roles-table.sql
+++
@@ -1,5 +0,0 @@
-CREATE TABLE roles (
-     user_id UUID REFERENCES users(user_id),
-     role_id UUID REFERENCES roles(role_id),
-     PRIMARY KEY(user_id, role_id)
-);
--- homework-6/spring-orm/src/main/liquibase/changelogs/v0.0.2/sql/01.alter-roles-table.sql
+++
@@ -1,7 +0,0 @@
-CREATE TABLE roles (
-     role_id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
-     role_name VARCHAR(127)
-);
-ALTER TABLE roles {
-
-}
